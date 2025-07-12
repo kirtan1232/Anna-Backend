c42b233dff8dab8e1bc1f548f4e73afa8540025a
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-07-12 17:25:58 +0545

    aopp

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ connectDb();
 
 // CORS configuration
 app.use(cors({
-    origin: 'https://localhost:5173',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
