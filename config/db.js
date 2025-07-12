48b9d9b77281560775720d652e7bbe6cb5f4f8c8
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-07-12 17:26:08 +0545

    dab

--- config/db.js
+++ config/db.js
@@ -1,16 +1,11 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI, {
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
-        });
-        console.log('MongoDB Connected');
+        await mongoose.connect("mongodb://localhost:27017/db_anna");
+        console.log("MongoDB Connected");
     } catch (e) {
-        console.error('MongoDB not connected:', e);
-        process.exit(1); // Exit process on connection failure
+        console.log("MongoDB not connected", e);
     }
 };
 
